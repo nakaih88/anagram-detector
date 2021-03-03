0b067dfd9d879137ea6f4cb69e7d53a4f8bbd521
Nakai <nakai.hamlin18@gmail.com>
2021-03-03 01:13:51 -0500

    commit tree be6bceabad66bb1f792bc7c94162753daf4c6e95

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        anagram = []
+        words.each do |value|
+            if value.split("").sort == word.split("").sort
+                anagram << value
+            end
+        end
+        anagram
+    end
+end
